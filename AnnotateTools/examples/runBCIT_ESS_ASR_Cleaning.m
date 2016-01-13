e587fa8464493e70ab1f48c5f5eefa9f48754588
Kyung Su <kmsu80@gmail.com>
2016-01-12 20:35:12 -0600

    Initial commit by Kyung

---
+++ AnnotateTools/examples/runBCIT_ESS_ASR_Cleaning.m
@@ -0,0 +1,21 @@
+%% Example using ESS for the BCIT to perform ASR cleaning
+% ess2Dir = 'O:\ARL_Data\BCIT_ESS\Experiment X2 Traffic Complexity';
+% ess2File = [ess2Dir filesep 'studyLevel2_description.xml'];
+% 
+% level2File = 'studyLevelDerived_description.xml';
+% levelDerivedDir = 'O:\ARL_Data\BCIT_ESS_256Hz\Experiment X2 Traffic Complexity';
+% levelDerivedDirNew = 'O:\ARL_Data\BCIT_ESS_256Hz_ICA\Experiment X2 Traffic Complexity';
+
+level2File = 'studyLevelDerived_description.xml';
+levelDerivedDir = 'Z:\Data 3\BCIT_ESS\Level2_256Hz\Experiment X2 Traffic Complexity';
+levelDerivedDirNew = 'Z:\Data 3\BCIT_ESS\Level2_256Hz_ASR\Experiment X2 Traffic Complexity';
+
+%% Make sure level 2 derived study validates
+derivedXMLFile = [levelDerivedDir filesep level2File];
+obj = levelDerivedStudy('parentStudyXmlFilePath', derivedXMLFile);
+
+%% Call the HP and ICA combination
+callbackAndParameters = {@cleanASR3, {'burstCriterion', 20}};    
+obj = obj.createLevelDerivedStudy(callbackAndParameters, ...
+      'filterDescription', 'Use ASR to clean the data', ...
+     'filterLabel', 'ASR', 'levelDerivedFolder', levelDerivedDirNew);
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # EEG-Annotate
 Tools for annotating continuous EEG signals
+
+# To run examples under the example directory
+- add path to EEGLAB (tested with 13.5.4b)
+- run EEGLAB, if clean_rawdata plugin is not installed, install it (version 0.31)
+- add path to Ess_tools
+- add path to AnnotateTools with Subfolders
+- update path in the script to input data
+- run (tested with MATLAB R2015b)
+
